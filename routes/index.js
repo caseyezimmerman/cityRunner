a9488460c9f916082732a9ef22657fb96f648727
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-11-08 14:31:02 -0500

    map

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var config = require ('../config/config')
+
+var request = require('request')
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+			
+		  res.render('index', { 
+		  	title: 'Express' ,
+		  });
+	
 });
 
 module.exports = router;
