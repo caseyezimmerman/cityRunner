1a9ed4ec88ccd2364187b035b1ae57894c80806b
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-11-09 10:01:44 -0500

    login/register

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt-nodejs');
+var request = require('request');
+var mysql = require('mysql');
+var config = require('../config/config')
+var connection = mysql.createConnection(config.db);
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,7 +14,9 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/register',(req,res,next)=>{
-	res.render('register',{})
+	res.render('register',{
+		title: 'City Runner',
+	})
 });
 
 router.post('/registerProcess',(req,res,next)=>{
@@ -22,7 +30,7 @@ router.post('/registerProcess',(req,res,next)=>{
 			res.redirect('/register')
 		}else{
 			var hash = bcrypt.hashSync(password)
-			var insertQuery = 'INSERT INTO users(name,email,password) VALUES (?,?,?);';
+			var insertQuery = 'INSERT INTO users (name,email,password) VALUES (?,?,?);';
 			connection.query(insertQuery,[name,email,hash],(error)=>{
 				if(error){
 					throw error
@@ -31,7 +39,37 @@ router.post('/registerProcess',(req,res,next)=>{
 			});
 		};
 	});
-
 });
+
+/* LOGIN */
+router.get('/login',(req,res,next)=>{
+	res.render('login',{
+		title: 'City Runner',
+	});
+});
+
+router.post('/loginProcess',(req,res,next)=>{
+	var email = req.body.email
+	var password = req.body.password
+	var selectQuery = 'SELECT * FROM users WHERE email = ?;';
+	connection.query(selectQuery,[email],(error,results)=>{
+		if(error){
+			throw error
+		} else {
+			if(results.length == 0){
+				res.redirect('/register')
+			} else {
+				var passwordMatch = bcrypt.compareSync(password,results[0].password)
+				if(passwordMatch){
+					// Add session?
+					res.redirect('/?msg=loggedIn')
+				} else {
+					res.redirect('/users/login?msg=passwordIncorrect');
+				}
+			}
+		}
+	})
+})
+
 
 module.exports = router;
