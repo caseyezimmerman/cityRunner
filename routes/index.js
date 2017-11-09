865f6678e842f3aec96e5d42bc900bd4003fdfa3
chris-malloy <chrismalloymusic@gmail.com>
2017-11-09 13:06:21 -0500

    made history route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var config = require ('../config/config');
+var config = require('../config/config');
 var bcrypt = require('bcrypt-nodejs');
 var request = require('request');
 var mysql = require('mysql');
@@ -12,30 +12,43 @@ var connection = mysql.createConnection(config.db);
 
 
 /* GET home page. */
-router.get('/',(req, res, next)=>{
-			
-		  res.render('index', { 
-		  	title: 'Express' ,
-		  });
-	
+router.get('/', (req, res, next) => {
+
+    res.render('index', {
+        title: 'Express',
+    });
+
 });
 
-router.get('/home',(req,res,next)=>{
-	res.render('home',{})
+router.get('/home', (req, res, next) => {
+    res.render('home', {})
 });
 
-router.post('/routesProcess',(req,res,next)=>{
-	var currentLocation = req.body.currentLocation
-	console.log("hey chris")	
-	var insertQuery = 'INSERT INTO routes (currentLocation) VALUES (?);';
-	connection.query(insertQuery,[currentLocation])	
-	if(error){
-		throw error
-	}else{
-		res.redirect('/home?msg=done')
-	}
+router.post('/routesProcess', (req, res, next) => {
+    var currentLocation = req.body.currentLocation
+    console.log("hey chris")
+    var insertQuery = 'INSERT INTO routes (currentLocation) VALUES (?);';
+    connection.query(insertQuery, [currentLocation])
+    if (error) {
+        throw error
+    } else {
+        res.redirect('/home?msg=done')
+    }
 });
 
-
+/* HISTORY */
+router.get('/history', (req, res, next) => {
+    var email = email;
+    var historyQuery = "SELECT * FROM history;"; // need to add in where clause to specify user
+    connection.query(historyQuery, [email], (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            res.render('history', {
+                runData: 'test',
+            })
+        }
+    })
+})
 
 module.exports = router;
