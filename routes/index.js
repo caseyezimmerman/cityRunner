50a46f58c7153acf60dd3f436499da623281642c
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-11-09 09:18:11 -0500

    cityrunner

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var router = express.Router();
-var config = require ('../config/config')
+var config = require ('../config/config');
+var bcrypt = require('bcrypt-nodejs');
+var request = require('request');
+var mysql = require('mysql');
 
-var request = require('request')
-
+var connection = mysql.createConnection(config.db);
+console.log(connection)
 
 
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',(req, res, next)=>{
 			
 		  res.render('index', { 
 		  	title: 'Express' ,
 		  });
 	
 });
+
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/register',(req,res,next)=>{
+	res.render('register',{})
+});
+
+router.post('/registerProcess',(req,res,next)=>{
+	var name = req.body.name
+	var email = req.body.email
+	var password = req.body.password
+	var selectQuery = 'SELECT * FROM users WHERE email = ?;';
+	connection.query(selectQuery,[email],(error,results)=>{
+		console.log(results)
+		if(results.length != 0){
+			res.redirect('/register')
+		}else{
+			var hash = bcrypt.hashSync(password)
+			var insertQuery = 'INSERT INTO users(name,email,password) VALUES (?,?,?);';
+			connection.query(insertQuery,[name,email,hash],(error)=>{
+				if(error){
+					throw error
+				}
+				res.redirect('/')
+			});
+		};
+	});
+
+});
+
 module.exports = router;
