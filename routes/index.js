610a9edd86ed779ae192dd5862fd8020e8c415e1
chris-malloy <chrismalloymusic@gmail.com>
2017-11-13 09:50:49 -0500

    added in session code

--- routes/index.js
+++ routes/index.js
@@ -9,56 +9,45 @@ var app = require('../app');
 
 var connection = mysql.createConnection(config.db);
 
-
-
-
-
 /* GET home page. */
 router.get('/', (req, res, next) => {
-
     res.render('index', {
-        title: 'Express',
+        title: 'City Runner',
     });
-
 });
 
 router.get('/home', (req, res, next) => {
-    res.render('home', {})
+    res.render('home', {
+        name: req.session.name,
+        loggedIn: true,
+    })
 });
 
+/* LOGOUT */
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
+})
+
+/* ROUTES */
 router.post('/routesProcess', (req, res, next) => {
     var currentLocation = req.body.currentLocation
     var distance = req.body.distance
     var userid = 2
     console.log("hey chris")
     var insertQuery = 'INSERT INTO routes (currentLocation,distance,userid) VALUES (?,?,?);';
-    connection.query(insertQuery, [currentLocation,distance,userid],(error)=>{
-    if (error) {
-        throw error
-    } else {
-    	console.log(distance)
-    	res.render('map',{
-    		address:currentLocation,
-    		distance:distance
-		});
-    }
-	});
-});
-
-router.post('/historyProcess',(req,res,next)=>{
-    var selectQuery = ' SELECT currentLocation,distance FROM routes WHERE userid = ?;';
-    connection.query(selectQuery,[userid],(error,results)=>{
-        if (error){
+    connection.query(insertQuery, [currentLocation, distance, userid], (error) => {
+        if (error) {
             throw error
-        }else{
-            res.render('map',{
-            address:currentLocation,
-            distance:distance
+        } else {
+            console.log(distance)
+            res.render('map', {
+                address: currentLocation,
+                distance: distance
             });
         }
-
-    })
-})
+    });
+});
 
 /* HISTORY */
 router.get('/history', (req, res, next) => {
@@ -68,34 +57,25 @@ router.get('/history', (req, res, next) => {
         if (error) {
             throw error;
         } else {
-            res.render('history', {results})
+            res.render('history', { results })
         }
     })
 })
 
-router.get('/map',(req,res,error)=>{
-	// res.render('map',{
-	// 	coords: {
-	// 		originLat:userLocationLatLng.lat,
-	// 		originLng:userLocationLatLng.lng
-	// 	},
-	// 	waypoints:{
-	// 		lat1:latArray[0],
-	// 		lng1:lngArray[0],
-	// 		lat2:latArray[1],
-	// 		lng2:lngArray[1],
-	// 		lat3:latArray[2],
-	// 		lng3:lngArray[2],
-	// 		lat4:latArray[3],
-	// 		lng4:lngArray[3],
-	// 		lat5:latArray[4],
-	// 		lng5:lngArray[4],
-	// 		lat6:latArray[5],
-	// 		lng6:lngArray[5]
-	// 	}
-	// })
-})
+router.post('/historyProcess', (req, res, next) => {
+    var selectQuery = ' SELECT currentLocation,distance FROM routes WHERE userid = ?;';
+    connection.query(selectQuery, [userid], (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            res.render('map', {
+                address: currentLocation,
+                distance: distance
+            });
+        }
 
+    })
+})
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -9,70 +9,70 @@ var connection = mysql.createConnection(config.db);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
-
-router.get('/register',(req,res,next)=>{
-	res.render('register',{
-		title: 'City Runner',
-	})
+/* REGISTER */
+router.get('/register', (req, res, next) => {
+    res.render('register', {
+        title: 'City Runner',
+    });
 });
 
-router.post('/registerProcess',(req,res,next)=>{
-	var name = req.body.name
-	var username = req.body.username
-	var email = req.body.email
-	var password = req.body.password
-	var selectQuery = 'SELECT * FROM users WHERE email = ?;';
-	connection.query(selectQuery,[email],(error,results)=>{
-		console.log(results)
-		if(results.length != 0){
-			res.redirect('/register')
-		}else{
-			var hash = bcrypt.hashSync(password)
-			var insertQuery = 'INSERT INTO users (name,username,email,password) VALUES (?,?,?,?);';
-			connection.query(insertQuery,[name,username,email,hash],(error)=>{
-				if(error){
-					throw error
-				}
-				res.redirect('/')
-			});
-		};
-	});
+router.post('/registerProcess', (req, res, next) => {
+    var name = req.body.name
+    var username = req.body.username
+    var email = req.body.email
+    var password = req.body.password
+    var selectQuery = 'SELECT * FROM users WHERE email = ?;';
+    connection.query(selectQuery, [email], (error, results) => {
+        console.log(results)
+        if (results.length != 0) {
+            res.redirect('/register')
+        } else {
+            var hash = bcrypt.hashSync(password)
+            var insertQuery = 'INSERT INTO users (name,username,email,password) VALUES (?,?,?,?);';
+            connection.query(insertQuery, [name, username, email, hash], (error) => {
+                if (error) {
+                    throw error
+                }
+                res.redirect('/')
+            });
+        };
+    });
 });
 
 /* LOGIN */
-router.get('/login',(req,res,next)=>{
-	res.render('login',{
-		title: 'City Runner',
-	});
+router.get('/login', (req, res, next) => {
+    res.render('login', {
+        title: 'City Runner',
+    });
 });
 
-
-
-router.post('/loginProcess',(req,res,next)=>{
-	var username = req.body.username
-	var password = req.body.password
-	var selectQuery = 'SELECT * FROM users WHERE username = ?;';
-	connection.query(selectQuery,[username],(error,results)=>{
-		if(error){
-			throw error
-		} else {
-			if(results.length == 0){
-				res.redirect('/register')
-			} else {
-				var passwordMatch = bcrypt.compareSync(password,results[0].password)
-				if(passwordMatch){
-					// Add session?
-					res.redirect('/home?msg=loggedIn')
-				} else {
-					res.redirect('/users/login?msg=passwordIncorrect');
-				}
-			}
-		}
-	})
+router.post('/loginProcess', (req, res, next) => {
+    var username = req.body.username
+    var password = req.body.password
+    var selectQuery = 'SELECT * FROM users WHERE username = ?;';
+    connection.query(selectQuery, [username], (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            if (results.length == 0) {
+                res.redirect('/register')
+            } else {
+                var passwordMatch = bcrypt.compareSync(password, results[0].password)
+                if (passwordMatch) {
+                    var row = results[0];
+                    req.session.name = row.name;
+                    req.session.uid = row.id;
+                    req.session.email = row.email;
+                    res.redirect('/home?msg=loggedIn')
+                } else {
+                    res.redirect('/users/login?msg=passwordIncorrect');
+                }
+            }
+        }
+    })
 })
-
 
 module.exports = router;
